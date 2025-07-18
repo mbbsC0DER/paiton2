6f7ab0a3fcca5d3453d9483074ce6282518f7f24
mbbsC0DER <kprathamnegi05@gmail.com>
2025-07-18 09:22:16 +0530

    commit for practical 1

--- pract1.py
+++ pract1.py
@@ -1,6 +1,52 @@
 print("helllo world")
+"""
+practical 1a
+
 num = int(input ("Ebter a number : "))
 if (num % 5 ==0 and num%11 == 0):
     print (num , "is divisible by both 5 and 11 .")
 else:
     print (num , "is not divisible by 5 and 11 .")
+"""
+"""practical 2a
+
+marks = int (input ("ENTER YOUR MARKS : ")) 
+if (marks < 0 or marks > 100 ):
+    print ("ERROR , Plz enter valid marks ! ")
+else :
+    if (marks >= 90):
+        print ("Congrats , you got grade O .")
+    elif marks >= 80 :
+        print ("You got grade A .")
+    elif marks >= 60 :
+        print ("You got grade B .")
+    elif marks >= 35 :
+        print ("You got grade C .")
+    else :
+        print ("You failed...")
+"""
+"""
+practical 1c
+n = int (input("Enter a number to count factorial : "))
+fact =  1 ;
+for i  in range(1 , n + 1):
+    fact *= i
+    print (i)
+print ("The factorial of the number is" , fact )
+"""
+"""
+practical 1d
+for i in range(99999999999999) :
+    n = int (input ("Enter any number except 0 : "))
+    if(n == 0):
+        print ("You entered 0 , so bye .")
+        break 
+    else :
+        print (n)
+"""
+
+for i in range (4 , 0 , -1):
+    for j in range (1 , i + 1):
+        print (i , end = " ")
+    print ()
+    
