1e626f1af5b73600b676dd9dfd472ebcde81296c
mbbsC0DER <kprathamnegi05@gmail.com>
2025-07-18 08:08:08 +0530

    commit for practical 1a

---
+++ pract1.py
@@ -0,0 +1,6 @@
+print("helllo world")
+num = int(input ("Ebter a number : "))
+if (num % 5 ==0 and num%11 == 0):
+    print (num , "is divisible by both 5 and 11 .")
+else:
+    print (num , "is not divisible by 5 and 11 .")
