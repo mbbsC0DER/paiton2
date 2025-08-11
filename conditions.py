90f35cc41f9dcf4ca757c1eacb0bce71a3d46417
mbbsC0DER <kprathamnegi05@gmail.com>
2025-08-11 10:57:03 +0530

    conditions final

--- conditions.py
+++ conditions.py
@@ -75,10 +75,11 @@ elif chars <= 10 :
     print ("thats medium .")
 else :
     print ("damn thats strong ,")
-"""
+
 
 year = int (input("enter year : "))
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0 ):
     print (f"{year} is a leap year .")
 else :
     print (" not a leap year .")
+"""
